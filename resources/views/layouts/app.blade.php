3784a93038cb5d440b48573082cdd4988fe98cbb
dimasdaffa <dimas.de2004@gmail.com>
2025-01-24 11:06:39 +0700

    session,error,flash message 22

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Task List App</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
-    <div>@yield('content')</div>
+    <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+        @yield('content')
+    </div>
 </body>
 
 </html>
