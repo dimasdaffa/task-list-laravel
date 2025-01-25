5aa40baf2c91fed97450e09464250a675f77e8b8
dimasdaffa <dimas.de2004@gmail.com>
2025-01-25 20:15:55 +0700

    tailwind task page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,40 +2,43 @@
 
 @section('title', $task->title)
 @section('content')
+    <div class="mb-4">
+        <a href="{{ route('tasks.index') }}" class="link">
+            <- Back to the list of tasks </a>
+    </div>
 
-    <p>{{ $task->description }}</p>
+    <p class="mb-4 text-slate-700">{{ $task->description }}</p>
     @if ($task->long_description)
-        <p>{{ $task->long_description }}</p>
+        <p class="mb-4 text-slate-700">{{ $task->long_description }}</p>
     @endif
-    <p>{{ $task->created_at }}</p>
-    <p>{{ $task->updated_at }}</p>
-    <p>
+    <p class="mb-4 text-sm text-slate-500">
+        Created {{ $task->created_at->diffForHumans() }} • Updated {{ $task->updated_at->diffForHumans() }}
+    </p>
+    <p class="mb-4">
         @if ($task->completed)
-            <span>Completed</span>
-            @else
-            <span>Incomplete</span>
+            <span class="font-medium text-green-500">Completed</span>
+        @else
+            <span class="font-medium text-red-500">Incomplete</span>
         @endif
     </p>
 
-    <div>
-        <a href="{{ route('tasks.edit', ['task' => $task->id]) }}">Edit Task</a>
-    </div>
+    <div class="flex gap-2">
+        <a href="{{ route('tasks.edit', ['task' => $task->id]) }} "
+            class="btn">
+            Edit</a>
 
-    <div>
-        <form method="POST" action="{{route('tasks.toggle-complete',['task' => $task])}}">
+        <form method="POST" action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
             @csrf
             @method('PUT')
-            <button type="submit">
+            <button class="btn" type="submit">
                 Mark As {{ $task->completed ? 'Incomplete' : 'Complete' }}
             </button>
         </form>
-    </div>
 
-    <div>
         <form action="{{ route('tasks.destroy', ['task' => $task]) }}" method="POST">
             @csrf
             @method('DELETE')
-            <button type="submit">Delete Task</button>
+            <button class="btn" type="submit">Delete Task</button>
         </form>
     </div>
 @endsection
